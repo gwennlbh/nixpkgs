9bf80d1f672c55e0db2450c5ec5dbea94acfe5d0
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-20 17:09:28 +0200

    make master-board build!

--- pkgs/by-name/od/odri-masterboard/package.nix
+++ pkgs/by-name/od/odri-masterboard/package.nix
@@ -3,18 +3,21 @@
   fetchgit,
   stdenv,
   cmake,
+  python312,
+  python312Packages,
 }:
 
 stdenv.mkDerivation rec {
   pname = "odri-masterboard";
-  version = "1.0.7";
+  version = "1c152c810dbb5e8c7bea0a3139b06e9d2286ad72";
 
   src = fetchgit {
-    url = "https://github.com/open-dynamic-robot-initiative/master-board";
+    #url = "https://github.com/open-dynamic-robot-initiative/master-board";
+    url = "https://github.com/gwennlbh/master-board";
     # repo = "odri_control_interface";
-    rev = "v${version}";
+    rev =version;
     # sha256 = "sha256-jeZFgAW8C+UCeW+TtV1fNJZR7yuWcpiQnepsbkNanPg=";
-    hash = "sha256-8Yw79we53PE9HHu2j95DR4PJi/MZVtunpFA2at7+4Uk=";
+    hash = "sha256-3vx3bOQkFnZXZ4jSh89Se7T8t6VpfL+JV3sVHz8+X78=";
     sparseCheckout = [
       "sdk/master_board_sdk"
     ];
@@ -23,5 +26,5 @@ stdenv.mkDerivation rec {
 
   sourceRoot = "${src.name}/sdk/master_board_sdk";
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake python312 python312Packages.boost ];
 }
