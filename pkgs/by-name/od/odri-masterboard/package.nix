0c8a076f352523da1bf63f0b31c9142a42bb3025
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-21 08:58:45 +0200

    masterboard: switch back to upstream

--- pkgs/by-name/od/odri-masterboard/package.nix
+++ pkgs/by-name/od/odri-masterboard/package.nix
@@ -9,14 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "odri-masterboard";
-  version = "1c152c810dbb5e8c7bea0a3139b06e9d2286ad72";
+  version = "9388c843e569d5bd3cd836f442ddfef43c8a7528";
 
   src = fetchgit {
-    #url = "https://github.com/open-dynamic-robot-initiative/master-board";
-    url = "https://github.com/gwennlbh/master-board";
-    # repo = "odri_control_interface";
+    url = "https://github.com/open-dynamic-robot-initiative/master-board";
     rev = version;
-    # sha256 = "sha256-jeZFgAW8C+UCeW+TtV1fNJZR7yuWcpiQnepsbkNanPg=";
     hash = "sha256-3vx3bOQkFnZXZ4jSh89Se7T8t6VpfL+JV3sVHz8+X78=";
     sparseCheckout = [
       "sdk/master_board_sdk"
